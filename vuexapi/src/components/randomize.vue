d5c6bbbac95ef120130cb15bfcf9e5cd6b423c8b
MikeKowalsky <mkowalewski83@gmail.com>
2019-03-12 16:30:23 +0100

    more improvements

--- vuexapi/src/components/randomize.vue
+++ vuexapi/src/components/randomize.vue
@@ -2,16 +2,21 @@
   <div class="randomize">
  
     <h1>Hey I am here</h1>
-          <p>{{currentBgPath.name}}</p>
+          <!-- <p>{{currentBgPath.name}}</p>
 
     <div v-for="(beer,index) in currentBgPath" :key="index">
       {{beer.name}}
 
     </div>
-    <button @click="randomize()"></button>
+    <button @click="randomize()"></button> -->
+    <div v-if="randomElement !== null">
+      <p>{{randomElement.tagline}}</p>
+      <p>{{randomElement.name}}</p>
+      <img :src="randomElement.image_url" alt="image">
+    </div>
+    <p v-else>Loading ..... Please wait</p>
 
   </div>
-
 </template>
 
 <script>
@@ -21,46 +26,38 @@ const _ = require("lodash");
 import { mapState } from "vuex";
 export default {
   name: "Randomize",
-  data(){
-    return{
-      currentBgPath:1,
+  data() {
+    return {
+      currentBgPath: 1,
+      randomElement: null
       // randomize:""
-    }
+    };
   },
 
- mounted() {
-   
+  mounted() {
+    this.randomize();
     this.$store.dispatch("loadCoins");
-    this.randomize;
-    
+
     // this.test();
-   
-  
   },
-   computed: {
+  computed: {
     ...mapState(["beers", "isLoaded"]),
     consoleMyLog() {
       return this.isLoaded;
-    },
-   
-  
-   },
-    methods:{
-    randomize(){
-        var random = Math.floor(Math.random() * this.$store.state.beers.length);
-      console.log(random);
-      this.currentBgPath = this.$store.state.beers[random];
-      console.log(
-        "Yo i am a random beer",
-        (this.currentBgPath = this.$store.state.beers[random])
-      );
-      }
-  
     }
-
-
-
-}
-
-
+  },
+  methods: {
+    randomize() {
+      var random = Math.floor(Math.random() * this.$store.state.beers.length);
+      console.log(random);
+      console.log(this.$store.state.beers);
+      // this.currentBgPath = this.$store.state.beers[random];
+      // console.log(
+      //   "Yo i am a random beer",
+      //   (this.currentBgPath = this.$store.state.beers[random])
+      // );
+      this.randomElement = this.$store.state.beers[random];
+    }
+  }
+};
 </script>
